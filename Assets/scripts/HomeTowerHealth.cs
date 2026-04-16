d8553b64bdb79a5e1ffaca6c7b14782a8accfdb1
MichaelwDuda <mwd20001@gmail.com>
2026-04-16 19:57:08 -0400

    home animation

--- Assets/scripts/HomeTowerHealth.cs
+++ Assets/scripts/HomeTowerHealth.cs
@@ -5,9 +5,11 @@ public class HomeTowerHealth : MonoBehaviour
     public int maxHealth = 100;
     private int currentHealth;
 
+    private Vector3 originalScale;
     void Start()
     {
         currentHealth = maxHealth;
+        originalScale = transform.localScale;
     }
 
     public void TakeDamage(int amount)
@@ -20,6 +22,18 @@ public class HomeTowerHealth : MonoBehaviour
         {
             Die();
         }
+    }
+
+    void UpdateTowerHeight()
+    {
+        float healthPercent = (float)currentHealth / maxHealth;
+
+        // Only scale the height (Y axis)
+        transform.localScale = new Vector3(
+            originalScale.x,
+            originalScale.y * healthPercent,
+            originalScale.z
+        );
     }
 
     void Die()
